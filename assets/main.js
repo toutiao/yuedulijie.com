6b9ce43f0a13439a706a822fe64a4d9052e349b2
Liu Lantao <liulantao@gmail.com>
2020-05-04 09:16:12 +0800

    sticky-top
    Signed-off-by: Liu Lantao <liulantao@gmail.com>

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,14 @@
 ---
 layout: nil
 ---
+
+var target = document.getElementsByClassName("sticky-top")[0];
+
+document.onscroll = function(e){
+  var percent = Math.max(document.body.scrollTop, document.documentElement.scrollTop) / 100;
+
+  if (percent > 0.95) { percent = 0.95; }
+  if (percent < 0) { percent = 0.0; }
+
+  target.style.backgroundColor = 'rgba(255, 255, 255, ' + percent + ')';
+};
